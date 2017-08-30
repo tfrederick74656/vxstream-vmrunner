b9b95fcc7a33c37c530d65ae16b24317258b1dbe
Tyler James Frederick <tfrederick74656@gmail.com>
2017-08-30 11:16:58 -0400

    Migrate Remaining Functionality to Perl

--- vmrun.pl
+++ vmrun.pl
@@ -1,6 +1,6 @@
 #########################################################################
 # vmrun Wrapper for VxStream Sandbox					#
-# Version 0.80, Updated 08/29/2018					#
+# Version 1.00, Updated 08/29/2018					#
 # © Tyler Frederick (tyler.frederick@securityriskadvisors.com)		#
 #########################################################################
 
@@ -8,56 +8,86 @@ use strict;
 use warnings;
 use feature "switch";
 no if ($] >= 5.018), 'warnings' => 'experimental';
-print "\n";
 
-# Declare Variables
-my ($args, $hostType, $hostName, $hostUser, $hostPass, $guestUser, $guestPass, $cmdName, $cmdParm, $vmxPath);
-$args = "", $hostType = "", $hostName = "", $hostUser = "", $hostPass = "", $guestUser = "", $guestPass = "", $cmdName = "", $cmdParm = "", $vmxPath = "";
+my $vmrPath = "/usr/bin/vmrun";
+my $logFile = "/home/vxstream/vmrunner.log";
+
+my ($inpArgs, $athFlgs, $cmdName, $vmxPath, $cmdParm);
+my $cmdList = "start|stop|reset|suspend|pause|unpause|listSnapshots|snapshot|deleteSnapshot|revertToSnapshot|runProgramInGuest|fileExistsInGuest|directoryExistsInGuest|setSharedFolderState|addSharedFolder|removeSharedFolder|enableSharedFolders|disableSharedFolders|listProcessesInGuest|killProcessInGuest|runScriptInGuest|deleteFileInGuest|createDirectoryInGuest|deleteDirectoryInGuest|createTempfileInGuest|listDirectoryInGuest|copyFileFromHostToGuest|copyFileFromGuestToHost|renameFileInGuest|captureScreen|writeVariable|readVariable|getGuestIPAddress|vprobeVersion|vprobeLoad|vprobeLoadFile|vprobeReset|vprobeListProbes|vprobeListGLobals|list|upgradevm|installTools|checkToolsState|register|unregister|listRegisteredVM|deleteVM|clone";
 
 # Parse Input String
-$args .= "$_ " foreach @ARGV;
-if($args =~ /^-T (?<hostType>ws|server|server1|fusion|esx|vc|player) +-h (?<hostName>(?:25[0-5]\.|2[0-4][0-9]\.|[01]?[0-9]{1,2}\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]{1,2})|[a-zA-Z0-9](?:[-a-zA-Z0-9]{0,61}[a-zA-Z0-9])?) +-u (?<hostUser>[a-z][-a-z0-9_]*) +-p (?<hostPassword>[^ ]+)(?: +-gu (?<guestUser>[a-z][-a-z0-9_]*) +-gp (?<guestPassword>[^ ]+))? +(?i)(?<commandName>start|stop|reset|suspend|pause|unpause|listSnapshots|snapshot|deleteSnapshot|revertToSnapshot|runProgramInGuest|fileExistsInGuest|directoryExistsInGuest|setSharedFolderState|addSharedFolder|removeSharedFolder|enableSharedFolders|disableSharedFolders|listProcessesInGuest|killProcessInGuest|runScriptInGuest|deleteFileInGuest|createDirectoryInGuest|deleteDirectoryInGuest|CreateTempfileInGuest|listDirectoryInGuest|CopyFileFromHostToGuest|CopyFileFromGuestToHost|renameFileInGuest|captureScreen|writeVariable|readVariable|getGuestIPAddress|vprobeVersion|vprobeLoad|vprobeLoadFile|vprobeReset|vprobeListProbes|vprobeListGLobals|list|upgradevm|installTools|checkToolsState|register|unregister|listRegisteredVM|deleteVM|clone)(?-i)(?: +(?<vmxPath>.+?\.vm(?:x|dk)) *(?: (?<commandParameters>.+?))?)? *$/) {
-	$hostType = $+{hostType};
-	$hostName = $+{hostName};
-	$hostUser = $+{hostUser};
-	$hostPass = $+{hostPassword};	
-	$guestUser = $+{guestUser};
-	$guestPass = $+{guestPassword};
-	$cmdName = $+{commandName};
-	$cmdParm = $+{commandParameters};	
+$inpArgs .= "$_ " foreach @ARGV;
+if ($inpArgs =~ /^(?<athFlgs>.+) (?<cmdName>$cmdList)(?: +(?<vmxPath>.+?\.vm(?:x|dk)) *(?: (?<cmdParm>.+?))?)? *$/) {
+	$athFlgs = $+{athFlgs};
+	$cmdName = $+{cmdName};
 	$vmxPath = $+{vmxPath};
+	$cmdParm = $+{cmdParm};
 } else {
-	print "Error: The parameters included are incorrect. Verify that all arguments required for the speficied command are included.", "\n";
-	exit(1);
+	print "Error: Malformed Argument List", "\n";
+	exit (1);
 }
 
-# Build Output String - Essential Parameters
-my $output = "-T $hostType -h $hostName -u $hostUser -p $hostPass";
-if(defined $guestUser && $guestUser ne "" && defined $guestPass && $guestPass ne "") {$output .= " -gu $guestUser -gp $guestPass";}
-$output .= " $cmdName";
-if(lc($cmdName) ne "list" && lc($cmdName) ne "listregisteredvm") {
-	if($vmxPath =~ /(\[[a-zA-Z0-9]+\] [a-zA-Z0-9]+\/[a-zA-Z0-9]+)_[0-9]-[0-9]{6}\.vmdk/) {$vmxPath = "$1.vmx";}
-	if($vmxPath !~ /^\".+\"$/) {$vmxPath = addQuotes($vmxPath);}
-	$output .= " $vmxPath";
+
+# Build Output String
+my $output = "$athFlgs $cmdName";
+if (defined $vmxPath && $vmxPath =~ m/^(\[.+\] .+\/.+)_.+\.vmdk$/) {
+	$output .= " ".addQuotes("$1.vmx");
+}
+for ($cmdName) {
+	when (/start|stop|reset|suspend|pause|unpause|listSnapshots|enableSharedFolders|disableSharedFolders|listProcessesInGuest|killProcessInGuest|CreateTempfileInGuest|getGuestIPAddress|vprobeVersion|vprobeReset|vprobeListProbes|vprobeListGLobals|list|upgradevm|installTools|checkToolsState|register|unregister|listRegisteredVM|deleteVM/) {}
+	when (/start|stop|reset|suspend|listSnapshots|enableSharedSolders|disableSharedFolders|killProcessInGuest|getGuestIPAddress/) {
+		$output .= " $cmdParm";
+	}
+	when (/snapshot|revertToSnapshot|fileExistsInGuest|directoryExistsInGuest|removeSharedFolder|deleteFileInGuest|createDirectoryInGuest|deleteDirectoryInGuest|listDirectoryInGuest|captureScreen|vprobeLoad|vprobeLoadFile/) {
+		$output .= " ".addQuotes($cmdParm);
+	}
+	when (/copyFileFromHostToGuest|copyFileFromGuestToHost/) {
+		if ($cmdParm =~ /(?:^(\/.+) (C:.+)$|^(C:.+) (\/.+))/) {
+			$output .= " ".addQuotes($1)." ".addQuotes($2);
+		}
+	}
+	when (/runProgramInGuest/) {
+		if($cmdParm =~ /^(.+) (\/c C:.+)/) {
+			$output .= " $1 ".addQuotes($2);
+		}
+	}
+	default { 
+		print "Error: Command Not Implemented", "\n";
+		exit (2);
+	}
 }
 
-# Build Output String - Command-Specific Parameters
-for (lc($cmdName)) {
-	when ("list" || "listregisteredvm") {}
-	when ("pause" || "unpause" || "listprocessesinguest" || "createtempfileinguest" || "vprobeversion" || "vprobereset" || "vprobelistprobes" || "vprobelistglobals" || "upgradevm" || "installtools" || "checktoolsstate" || "register" || "unregister" || "deletevm") {}
-	when ("start" || "stop" || "reset" || "suspend" || "listsnapshots" || "enablesharedfolders" || "disablesharedfolders" || "killprocessinguest" || "getguestipaddress" || "snapshot" || "reverttosnapshot") { ... }
-	when ("deletesnapshot") { ... }
-	default { ... }
+my $return = `$vmrPath $output`;
+my $print = fixReturn($return);
+print $print, "\n";
+writeLog();
+exit (0);
+
+sub addQuotes {
+	my $input = "\"".shift."\"";
 }
 
-print $output, "\n";
-run();
-
-sub addQuotes {my $input = "\"".shift."\"";}
-sub run {
-	my $return = system("/usr/bin/vmrun $output");
-	print $return;
+sub fixReturn {
+	my $value = "";
+	my @lines = split /\n/, shift;
+	foreach my $line (@lines) {
+		$_ = $line;
+		print $line;
+		s/(\[)(.+\/)(.+\] .+\/.+)(\.vm)(x)/$1$3_0-000001$4dk/;
+		$value .= "$_\n";
+	}
+	print $value;
+	chomp $value;
+	print $value;
+	return $value;
 }
 
-#print "\n$output\n\n";
-print "\n\n";
+sub writeLog {
+	open(my $fh, '>>', $logFile) or die "Error: Logging Failed";
+	say $fh "Session Begin at ".localtime();
+	say $fh "INP: $inpArgs";
+	say $fh "OUT: $output";
+	say $fh "RTN: $return";
+	say $fh "PRT: ".shift; 
+	close $fh;
+}
