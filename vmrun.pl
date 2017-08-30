f9351de58d0b7ddc9cb6a2b42cf7439c4400b27f
Tyler James Frederick <tfrederick74656@gmail.com>
2017-08-30 11:22:52 -0400

    Major Cleanup and Log Support

--- vmrun.pl
+++ vmrun.pl
@@ -1,93 +1,140 @@
 #########################################################################
 # vmrun Wrapper for VxStream Sandbox					#
-# Version 1.00, Updated 08/29/2018					#
+# Version 2.00, Updated 08/29/2018					#
 # © Tyler Frederick (tyler.frederick@securityriskadvisors.com)		#
 #########################################################################
 
+# Global Modifiers
+require 5.010_001;
 use strict;
 use warnings;
-use feature "switch";
+use feature 'switch';
 no if ($] >= 5.018), 'warnings' => 'experimental';
 
-my $vmrPath = "/usr/bin/vmrun";
-my $logFile = "/home/vxstream/vmrunner.log";
+# Runtime Constants 
+use constant VMRUN_PATH => $ARGV[0];
+use constant LOG_FILE   => $ARGV[1];
+use constant LOG_LEVEL  => $ARGV[2];
+use constant LOG_LEVEL_NONE  => 0;
+use constant LOG_LEVEL_ERROR => 1;
+use constant LOG_LEVEL_WARN  => 2;
+use constant LOG_LEVEL_INFO  => 3;
+use constant LOG_LEVEL_DEBUG => 4;
+use constant COMMAND_LIST => 'start|stop|reset|suspend|pause|unpause|listSnapshots|snapshot|deleteSnapshot|revertToSnapshot|runProgramInGuest|fileExistsInGuest|directoryExistsInGuest|setSharedFolderState|addSharedFolder|removeSharedFolder|enableSharedFolders|disableSharedFolders|listProcessesInGuest|killProcessInGuest|runScriptInGuest|deleteFileInGuest|createDirectoryInGuest|deleteDirectoryInGuest|createTempfileInGuest|listDirectoryInGuest|copyFileFromHostToGuest|copyFileFromGuestToHost|renameFileInGuest|captureScreen|writeVariable|readVariable|getGuestIPAddress|vprobeVersion|vprobeLoad|vprobeLoadFile|vprobeReset|vprobeListProbes|vprobeListGLobals|list|upgradevm|installTools|checkToolsState|register|unregister|listRegisteredVM|deleteVM|clone';
+use constant CMDLIST_NOARGS => 'start|stop|reset|suspend|pause|unpause|listSnapshots|enableSharedFolders|disableSharedFolders|listProcessesInGuest|killProcessInGuest|CreateTempfileInGuest|getGuestIPAddress|vprobeVersion|vprobeReset|vprobeListProbes|vprobeListGLobals|list|upgradevm|installTools|checkToolsState|register|unregister|listRegisteredVM|deleteVM';
+use constant CMDLIST_STRAIGHTARGS => 'start|stop|reset|suspend|listSnapshots|enableSharedSolders|disableSharedFolders|killProcessInGuest|getGuestIPAddress';
+use constant CMDLIST_QUOTESONLY => 'snapshot|revertToSnapshot|fileExistsInGuest|directoryExistsInGuest|removeSharedFolder|deleteFileInGuest|createDirectoryInGuest|deleteDirectoryInGuest|listDirectoryInGuest|captureScreen|vprobeLoad|vprobeLoadFile';
+use constant CMDLIST_COPYFILE => 'copyFileFromHostToGuest|copyFileFromGuestToHost';
+use constant CMDLIST_RUNPROGRAM => 'runProgramInGuest';
 
-my ($inpArgs, $athFlgs, $cmdName, $vmxPath, $cmdParm);
-my $cmdList = "start|stop|reset|suspend|pause|unpause|listSnapshots|snapshot|deleteSnapshot|revertToSnapshot|runProgramInGuest|fileExistsInGuest|directoryExistsInGuest|setSharedFolderState|addSharedFolder|removeSharedFolder|enableSharedFolders|disableSharedFolders|listProcessesInGuest|killProcessInGuest|runScriptInGuest|deleteFileInGuest|createDirectoryInGuest|deleteDirectoryInGuest|createTempfileInGuest|listDirectoryInGuest|copyFileFromHostToGuest|copyFileFromGuestToHost|renameFileInGuest|captureScreen|writeVariable|readVariable|getGuestIPAddress|vprobeVersion|vprobeLoad|vprobeLoadFile|vprobeReset|vprobeListProbes|vprobeListGLobals|list|upgradevm|installTools|checkToolsState|register|unregister|listRegisteredVM|deleteVM|clone";
+# Initialize Logging
+my $log;
+if(LOG_LEVEL > LOG_LEVEL_NONE) {
+	open($log, '>>', LOG_FILE) or die "Error: Logging Failed\n";
+}
+logMsg (LOG_LEVEL_WARN, 'LOG', 'Session Started at '.localtime.". Log level: ".LOG_LEVEL);
+sub logMsg {
+        my ($msgLevel, $msgHeader, $msgBody) = @_;
+        chomp $msgBody;
+        if (defined $log && LOG_LEVEL >= $msgLevel) {
+		say $log time." $msgLevel $msgHeader: $msgBody";
+	}
+}
+sub logExit {
+        logMsg (LOG_LEVEL_WARN, 'LOG', 'Session Completed at '.localtime);
+        close $log;
+}
 
-# Parse Input String
-$inpArgs .= "$_ " foreach @ARGV;
-if ($inpArgs =~ /^(?<athFlgs>.+) (?<cmdName>$cmdList)(?: +(?<vmxPath>.+?\.vm(?:x|dk)) *(?: (?<cmdParm>.+?))?)? *$/) {
-	$athFlgs = $+{athFlgs};
+# Parse Arguments
+logMsg (LOG_LEVEL_DEBUG, 'PRS', 'Starting Phase: Parse Arguments');
+my $cmdList = COMMAND_LIST;
+my ($inputArgs, $athArgs, $cmdName, $vmxPath, $cmdArgs);
+$inputArgs .= "$_ " foreach @ARGV;
+logMsg (LOG_LEVEL_INFO, 'PRS', "Input arguments: $inputArgs");
+if ($inputArgs =~ /^[^ ]+ [^ ]+ \d (?<athArgs>.+) (?<cmdName>$cmdList)(?: +(?<vmxPath>.+?\.vm(?:x|dk)) *(?: (?<cmdArgs>.+?))?)? *$/) {
+	$athArgs = $+{athArgs};
 	$cmdName = $+{cmdName};
 	$vmxPath = $+{vmxPath};
-	$cmdParm = $+{cmdParm};
+	$cmdArgs = $+{cmdArgs};
+	if (defined $athArgs) { logMsg(LOG_LEVEL_DEBUG, 'PRS', "athArgs: $athArgs"); }
+	if (defined $cmdName) { logMsg(LOG_LEVEL_DEBUG, 'PRS', "cmdName: $cmdName"); }
+	if (defined $vmxPath) { logMsg(LOG_LEVEL_DEBUG, 'PRS', "vmxPath: $vmxPath"); }
+	if (defined $cmdArgs) { logMsg(LOG_LEVEL_DEBUG, 'PRS', "cmdArgs: $cmdArgs"); }
 } else {
-	print "Error: Malformed Argument List", "\n";
-	exit (1);
+	logMsg (LOG_LEVEL_ERROR, 'PRS', 'Malformed argument list. Exiting.');
+	logExit();
+	die "Error: Malformed Argument List\n";
 }
 
 
 # Build Output String
-my $output = "$athFlgs $cmdName";
-if (defined $vmxPath && $vmxPath =~ m/^(\[.+\] .+\/.+)_.+\.vmdk$/) {
-	$output .= " ".addQuotes("$1.vmx");
-}
-for ($cmdName) {
-	when (/start|stop|reset|suspend|pause|unpause|listSnapshots|enableSharedFolders|disableSharedFolders|listProcessesInGuest|killProcessInGuest|CreateTempfileInGuest|getGuestIPAddress|vprobeVersion|vprobeReset|vprobeListProbes|vprobeListGLobals|list|upgradevm|installTools|checkToolsState|register|unregister|listRegisteredVM|deleteVM/) {}
-	when (/start|stop|reset|suspend|listSnapshots|enableSharedSolders|disableSharedFolders|killProcessInGuest|getGuestIPAddress/) {
-		$output .= " $cmdParm";
-	}
-	when (/snapshot|revertToSnapshot|fileExistsInGuest|directoryExistsInGuest|removeSharedFolder|deleteFileInGuest|createDirectoryInGuest|deleteDirectoryInGuest|listDirectoryInGuest|captureScreen|vprobeLoad|vprobeLoadFile/) {
-		$output .= " ".addQuotes($cmdParm);
-	}
-	when (/copyFileFromHostToGuest|copyFileFromGuestToHost/) {
-		if ($cmdParm =~ /(?:^(\/.+) (C:.+)$|^(C:.+) (\/.+))/) {
-			$output .= " ".addQuotes($1)." ".addQuotes($2);
-		}
-	}
-	when (/runProgramInGuest/) {
-		if($cmdParm =~ /^(.+) (\/c C:.+)/) {
-			$output .= " $1 ".addQuotes($2);
-		}
-	}
-	default { 
-		print "Error: Command Not Implemented", "\n";
-		exit (2);
-	}
-}
-
-my $return = `$vmrPath $output`;
-my $print = fixReturn($return);
-print $print, "\n";
-writeLog();
-exit (0);
+logMsg (LOG_LEVEL_DEBUG, 'BOS', 'Starting Phase: Build Output String');
 
 sub addQuotes {
-	my $input = "\"".shift."\"";
+        my $input = "\"".shift."\"";
+	logMsg (LOG_LEVEL_DEBUG, 'QOT', "addQuotes returns $input");
+	return $input;
 }
-
-sub fixReturn {
-	my $value = "";
-	my @lines = split /\n/, shift;
-	foreach my $line (@lines) {
-		$_ = $line;
-		print $line;
-		s/(\[)(.+\/)(.+\] .+\/.+)(\.vm)(x)/$1$3_0-000001$4dk/;
-		$value .= "$_\n";
+my $outputArgs = "$athArgs $cmdName";
+logMsg (LOG_LEVEL_DEBUG, 'BOS', "Processing coreArgs: $outputArgs");
+if (defined $vmxPath && $vmxPath =~ m/^(\[.+\] .+\/.+)_.+\.vmdk$/) {
+	$outputArgs .= " ".addQuotes("$1.vmx");
+	logMsg (LOG_LEVEL_DEBUG, 'BOS', "Processing vmxPath: $outputArgs")
+}
+my $cmdListNoArgs = CMDLIST_NOARGS;
+my $cmdListStraightArgs = CMDLIST_STRAIGHTARGS;
+my $cmdListQuotesOnly = CMDLIST_QUOTESONLY;
+my $cmdListCopyFile = CMDLIST_COPYFILE;
+my $cmdListRunProgram = CMDLIST_RUNPROGRAM;
+for ($cmdName) {
+	when (/$cmdListNoArgs/) {
+		logMsg(LOG_LEVEL_DEBUG, 'BOS', "Processing cmdListNoArgs: $outputArgs");
 	}
-	print $value;
-	chomp $value;
-	print $value;
-	return $value;
+	when (/$cmdListStraightArgs/) {
+		$outputArgs .= " $cmdArgs";
+		logMsg(LOG_LEVEL_DEBUG, 'BOS', "Processing cmdListStraightArgs: $outputArgs");
+	}
+	when (/$cmdListQuotesOnly/) {
+		$outputArgs .= " ".addQuotes($cmdArgs);
+		logMsg(LOG_LEVEL_DEBUG, 'BOS', "Processing cmdListQuotesOnly: $outputArgs");
+	}
+	when (/$cmdListCopyFile/) {
+		if ($cmdArgs =~ /(?:^(\/.+) (C:.+)$|^(C:.+) (\/.+))/) {
+			$outputArgs .= " ".addQuotes($1)." ".addQuotes($2);
+		}
+		logMsg(LOG_LEVEL_DEBUG, 'BOS', "Processing cmdListCopyFile: $outputArgs");
+	}
+	when (/$cmdListRunProgram/) {
+		if($cmdArgs =~ /^(.+) (\/c C:.+)/) {
+			$outputArgs .= " $1 ".addQuotes($2);
+		}
+		logMsg(LOG_LEVEL_DEBUG, 'BOS', "Processing cmdListRunProgram: $outputArgs");
+	}
+	default { 
+		logMsg(LOG_LEVEL_ERROR, 'BOS', "Command $cmdName not implemented. Exiting.");
+		logExit();
+		exit "Error: Command Not Implemented\n";
+	}
 }
+logMsg(LOG_LEVEL_INFO, 'BOS', "Output String: $outputArgs");
 
-sub writeLog {
-	open(my $fh, '>>', $logFile) or die "Error: Logging Failed";
-	say $fh "Session Begin at ".localtime();
-	say $fh "INP: $inpArgs";
-	say $fh "OUT: $output";
-	say $fh "RTN: $return";
-	say $fh "PRT: ".shift; 
-	close $fh;
+# Call vmrun and Patch Return
+logMsg (LOG_LEVEL_DEBUG, 'RUN', 'Starting run block');
+my $vmrPath = VMRUN_PATH;
+logMsg (LOG_LEVEL_INFO, 'RUN', "Executing vmrun: $vmrPath $outputArgs");
+my $returnIn = `$vmrPath $outputArgs`;
+logMsg (LOG_LEVEL_DEBUG, 'RUN', "vmrun returned: $returnIn");
+my $returnOut = "";
+my @lines = split /\n/, $returnIn;
+foreach my $line (@lines) {
+	$_ = $line;
+	s/(\[)(.+\/)(.+\] .+\/.+)(\.vm)(x)/$1$3_0-000001$4dk/;
+	$returnOut .= "$_\n";
 }
+chomp $returnOut;
+logMsg (LOG_LEVEL_INFO, 'RUN', "Return value: $returnOut");
+print $returnOut, "\n";
+
+# Cleanup and Exit
+logExit();
+exit 0;
--- vmrun.sh
+++ vmrun.sh
@@ -1,1 +1,13 @@
-/usr/bin/perl /home/vxstream/vmrun.pl $@
+#########################################################################
+# vmrun Wrapper for VxStream Sandbox                                    #
+# Version 2.00, Updated 08/29/2018                                      #
+# © Tyler Frederick (tyler.frederick@securityriskadvisors.com)          #
+#########################################################################
+
+readonly PERL_PATH="/usr/bin/perl"		# Path to the Perl binary (5.10.1 or newer)
+readonly VMRUN_PATH="/usr/bin/vmrun"		# Path to the VMware VIX API binary (vmrun)
+readonly SCRIPT_PATH="/home/vxstream/vmrun.pl"	# Path to the vmrun.pl script
+readonly LOG_PATH="/home/vxstream/vmrun.log"	# Path to a log file for the script
+readonly LOG_LEVEL="0"				# 0 = None, 1 = Error, 2 = Warning/Basic, 3 = Info, 4 = Debug
+readonly SCRIPT_ARGS=$@				# Argument list for the script
+$PERL_PATH "$SCRIPT_PATH" "$VMRUN_PATH" "$LOG_PATH" "$LOG_LEVEL" "$SCRIPT_ARGS"
